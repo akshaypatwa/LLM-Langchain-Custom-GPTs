8b9cf7fe5ff7d17bfcf70f4a3d3b90efd74efdfb
akshaypatwa <akshaypatwa74@gmail.com>
2024-01-01 15:19:52 +0530

    Initial Commit

---
+++ Text Reader GPT_OpenAI/constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
